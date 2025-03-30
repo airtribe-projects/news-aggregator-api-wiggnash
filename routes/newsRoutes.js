6fcf64533bd115b4d114f125aefd206d32c019f9
wiggnash <vigneshwar.sigamani@gmail.com>
2025-03-30 11:31:16 +0530

    update the news api call

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 // middlwares
+import validateToken from "../middleware/validateToken.js";
+
 // controllers
 import { getNews } from "../controllers/newsController.js";
 const newsRoutes = express.Router();
 
-newsRoutes.get("/", getNews);
+newsRoutes.get("/", validateToken, getNews);
 
 export default newsRoutes;
